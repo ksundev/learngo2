831d4001d88ee94a1887875591d20b39f12c9f34
ksundev <kshk08@hanyang.ac.kr>
2025-07-14 15:16:20 +0900

    refactor: url checker를 go routine과 결합

--- main.go
+++ main.go
@@ -2,21 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 func main() {
-	c := make(chan string)
-	people := [4]string{"KSun", "Peter", "John", "Chris"}
-	for _, person := range people {
-		go isReady(person, c)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
 	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println("Received:", <-c)
+
+	c := make(chan requestResult)
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		fmt.Println(result)
 	}
 }
 
-func isReady(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person + " is ready"
+func hitURL(url string, c chan<- requestResult) {
+	fmt.Println("Checking", url)
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- requestResult{url: url, status: status}
 }
